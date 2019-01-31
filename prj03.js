6ea939376d151dd6f55ee58813e3a47d235cfbf5
Zach Brundage <bru15012@byui.edu>
2019-01-31 16:50:39 -0700

    prj03 finished

--- prj03.js
+++ prj03.js
@@ -1,12 +1,13 @@
 function createUser() {
     
-    alert("CALLED");
+    // Get Input Values from HTML Form
     var firstName = document.getElementById("firstName").value;
     var lastName = document.getElementById("lastName").value;
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     var email = document.getElementById("email").value;
     
+    // Turn Input Into Object
     var userObj = {
         fName: firstName, 
         lName: lastName, 
@@ -15,6 +16,28 @@ function createUser() {
         eAddress: email
     };
     
+    // Convert Object to JSON
     var jsonFile = JSON.stringify(userObj);
-    localStorage.setItem("prj03JSON", jsonFile);
+    
+    // Push JSON to local storage
+    localStorage.setItem("JSON", jsonFile);
+    
+    displayJSON();
 }
+
+function displayJSON() {
+    
+    // Get JSON from local storage
+    var jsonFile = localStorage.getItem("JSON");
+    
+    // Parse JSON back to Object
+    var obj = JSON.parse(jsonFile);
+    
+    // Display via DOM
+    document.getElementById("fName").innerHTML = "First Name: " + obj.fName;
+    document.getElementById("lName").innerHTML = "Last Name: " + obj.lName;
+    document.getElementById("uName").innerHTML = "Username: " + obj.uName;
+    document.getElementById("pass").innerHTML = "Password: " + obj.pass;
+    document.getElementById("em").innerHTML = "Email: " + obj.eAddress;
+}
+
