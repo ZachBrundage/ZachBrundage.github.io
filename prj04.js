68ec46771f33504c163d4bf16941ab972ae8b8b6
Zach Brundage <bru15012@byui.edu>
2019-02-08 15:49:24 -0700

    Finish PRJ04

--- prj04.js
+++ prj04.js
@@ -1,19 +1,30 @@
-var xmlHttp = new XMLHttpRequest();
-var url = "prj04.txt";
+function sendRequest() {
+    // Create an XMLHttpRequest Object
+    var xmlHttp = new XMLHttpRequest();
+    var url = "prj04.txt";
 
-xmlHttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status ==200){
-        var gallery = JSON.parse(this.responseText);
-        display(gallery);
-    }
-};
-xmlHttp.open("GET", url, true);
-xmlHttp.send();
+    // Parse JSON from text file
+    xmlHttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200){
+            var gallery = JSON.parse(this.responseText);
+            display(gallery);
+        }
+    };
+    
+    // Send XMLHttpRequest
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send();
+}
 
 function display(gallery){
+    // Display Variable
     var displayGallery = "";
+    
+    // Loop through gallery object
     for (var i = 0; i < gallery.length; i++){
         displayGallery += '<img src="' + gallery[i].file + '">' + '<br>';
     }
+    
+    // Display on webpafe via DOM
     document.getElementById("display").innerHTML = displayGallery;
 }
