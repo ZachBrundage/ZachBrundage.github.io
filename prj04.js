dd046532fb4f4cf69769b346cbe1466976a796f0
Zach Brundage <bru15012@byui.edu>
2019-02-08 14:33:35 -0700

    prjo4

---
+++ prj04.js
@@ -0,0 +1,19 @@
+var xmlHttp = new XMLHttpRequest();
+var url = "prj04.txt";
+
+xmlHttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status ==200){
+        var galley = JSON.parse(this.responseText);
+        display(gallery);
+    }
+};
+xmlHttp.open("GET", url, true);
+xmlHttp.send();
+
+function display(gallery){
+    var displayGallery = "";
+    for (var i = 0; i < gallery.length; i++){
+        displayGallery += '<img src="' + gallery[i].file + '">' + '<br>';
+    }
+    document.getElementById("display").innerHTML = displayGallery;
+}
