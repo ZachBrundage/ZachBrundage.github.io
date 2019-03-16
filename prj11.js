7a3fcb099b998d721c9129d4b2bdc38a6f70cddd
Zach Brundage <bru15012@byui.edu>
2019-03-16 16:47:42 -0700

    prj11

---
+++ prj11.js
@@ -0,0 +1,101 @@
+function start() {
+    var recOne = document.getElementById("rec1");
+    var recTwo = document.getElementById("rec2");
+    var recThree = document.getElementById("rec3");
+    var recFour = document.getElementById("rec4");
+    var recFive = document.getElementById("rec5");
+    var recSix = document.getElementById("rec6");
+    var recSeven = document.getElementById("rec7");
+    var recEight = document.getElementById("rec8");
+    var recNine = document.getElementById("rec9");
+    var recTen = document.getElementById("rec10");
+    var recEleven = document.getElementById("rec11");
+    var recTwelve = document.getElementById("rec12");
+    
+    recOne.style.height = "150px";
+    recOne.style.bottom = "185px";
+    
+    recTwo.style.height = "150px";
+    recTwo.style.bottom = "185px";
+    
+    recThree.style.height = "150px";
+    recThree.style.bottom = "185px";
+    
+    recFour.style.height = "150px";
+    recFour.style.bottom = "185px";
+    
+    recFive.style.height = "150px";
+    recFive.style.bottom = "185px";
+    
+    recSix.style.height = "150px";
+    recSix.style.bottom = "185px";
+    
+    recSeven.style.height = "150px";
+    recSeven.style.bottom = "185px";
+    
+    recEight.style.height = "150px";
+    recEight.style.bottom = "185px";
+    
+    recNine.style.height = "150px";
+    recNine.style.bottom = "185px";
+    
+    recTen.style.height = "150px";
+    recTen.style.bottom = "185px";
+    
+    recEleven.style.height = "150px";
+    recEleven.style.bottom = "185px";
+    
+    recTwelve.style.height = "150px";
+    recTwelve.style.bottom = "185px";
+}
+
+function reverse() {
+    var recOne = document.getElementById("rec1");
+    var recTwo = document.getElementById("rec2");
+    var recThree = document.getElementById("rec3");
+    var recFour = document.getElementById("rec4");
+    var recFive = document.getElementById("rec5");
+    var recSix = document.getElementById("rec6");
+    var recSeven = document.getElementById("rec7");
+    var recEight = document.getElementById("rec8");
+    var recNine = document.getElementById("rec9");
+    var recTen = document.getElementById("rec10");
+    var recEleven = document.getElementById("rec11");
+    var recTwelve = document.getElementById("rec12");
+    
+    recOne.style.height = "50px";
+    recOne.style.bottom = "235px";
+    
+    recTwo.style.height = "50px";
+    recTwo.style.bottom = "235px";
+    
+    recThree.style.height = "50px";
+    recThree.style.bottom = "235px";
+    
+    recFour.style.height = "50px";
+    recFour.style.bottom = "235px";
+    
+    recFive.style.height = "50px";
+    recFive.style.bottom = "235px";
+    
+    recSix.style.height = "50px";
+    recSix.style.bottom = "235px";
+    
+    recSeven.style.height = "50px";
+    recSeven.style.bottom = "235px";
+    
+    recEight.style.height = "50px";
+    recEight.style.bottom = "235px";
+    
+    recNine.style.height = "50px";
+    recNine.style.bottom = "235px";
+    
+    recTen.style.height = "50px";
+    recTen.style.bottom = "235px";
+    
+    recEleven.style.height = "50px";
+    recEleven.style.bottom = "235px";
+    
+    recTwelve.style.height = "50px";
+    recTwelve.style.bottom = "235px";
+}
