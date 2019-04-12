9ca87d0984a8c436c8761a867fae800ac44fd474
Zach Brundage <bru15012@byui.edu>
2019-04-12 11:42:49 -0700

    UPDATE

--- basecamp.js
+++ basecamp.js
@@ -1,166 +1,241 @@
-function sendFranmarRequest() {
-    // Create an XMLHttpRequest Object
-    var xmlHttp = new XMLHttpRequest();
-    var url = "https://3.basecampapi.com/4191003/buckets/11511950/todolists/1689002219/todos.json?access_token=BAhbB0kiAbB7ImNsaWVudF9pZCI6IjBmN2U2NjJlMTZkNmMyMDNmOGYyZWFlOWQ3ZTZjYzU4NTRkNDYyMGQiLCJleHBpcmVzX2F0IjoiMjAxOS0wNC0wOVQyMDoyMTowM1oiLCJ1c2VyX2lkcyI6WzM5MTQ3Mzk5XSwidmVyc2lvbiI6MSwiYXBpX2RlYWRib2x0IjoiZWRlNThiNDk4MDhkNmFlMTQ2YjcyMDkzNjNlNGRjNDkifQY6BkVUSXU6CVRpbWUNNM0dwM3NO1QJOg1uYW5vX251bWkClAM6DW5hbm9fZGVuaQY6DXN1Ym1pY3JvIgeRYDoJem9uZUkiCFVUQwY7AEY=--e81cb35aefe218064d4ee49efd76d861a9dfa547";
+var plazaDataOne;
+var plazaDataTwo;
+var plazaDataThree;
+var plazaDataFour;
+var plazaDataFive;
+var plazaDataSix;
+var plazaDataSeven;
+var plazaDataEight;
 
-    // Parse JSON response
+localStorage.clear();
+
+function display(){
+    var str01 = localStorage.getItem("PlazaOne");
+    var str02 = localStorage.getItem("PlazaTwo");
+    var str03 = localStorage.getItem("PlazaThree");
+    var str04 = localStorage.getItem("PlazaFour");
+    var str05 = localStorage.getItem("PlazaFive");
+    var str06 = localStorage.getItem("PlazaSix");
+    var str07 = localStorage.getItem("PlazaSeven");
+    var str08 = localStorage.getItem("PlazaEight");
+    
+    var data01 = JSON.parse(str01);
+    var data02 = JSON.parse(str02);
+    var data03 = JSON.parse(str03);
+    var data04 = JSON.parse(str04);
+    var data05 = JSON.parse(str05);
+    var data06 = JSON.parse(str06);
+    var data07 = JSON.parse(str07);
+    var data08 = JSON.parse(str08);
+    
+    console.log(data01);
+    console.log(data02);
+    console.log(data03);
+    console.log(data04);
+    console.log(data05);
+    console.log(data06);
+    console.log(data07);
+    console.log(data08);
+}
+
+function callPlaza(){
+    getPlazaTodos();
+    getPlazaTodosTwo();
+    getPlazaTodosThree();
+    getPlazaTodosFour();
+    getPlazaTodosFive();
+    getPlazaTodosSix();
+    getPlazaTodosSeven();
+    getPlazaTodosEight();
+}
+
+function getPlazaTodos() {
+    
+    var xmlHttp = new XMLHttpRequest();
+    
+    var url = "https://3.basecampapi.com/4191003/projects/recordings.json?type=Todo&bucket=11349404&access_token=BAhbB0kiAbB7ImNsaWVudF9pZCI6IjBmN2U2NjJlMTZkNmMyMDNmOGYyZWFlOWQ3ZTZjYzU4NTRkNDYyMGQiLCJleHBpcmVzX2F0IjoiMjAxOS0wNC0yNVQyMTozNDo1OVoiLCJ1c2VyX2lkcyI6WzM5MTQ3Mzk5XSwidmVyc2lvbiI6MSwiYXBpX2RlYWRib2x0IjoiZWRlNThiNDk4MDhkNmFlMTQ2YjcyMDkzNjNlNGRjNDkifQY6BkVUSXU6CVRpbWUNNc8dwMiVtIsJOg1uYW5vX251bWkCUQE6DW5hbm9fZGVuaQY6DXN1Ym1pY3JvIgczcDoJem9uZUkiCFVUQwY7AEY=--2397b353eb46b3b3a25f9a8f3fe99ea9fb49534b";
+
     xmlHttp.onreadystatechange = function () {
+      
         if (this.status === 200){
-            var data = JSON.parse(this.response);
-            console.log(data);
-            display(data);
+            localStorage.setItem("PlazaOne", this.response);
         }
-        else if(this.status === 305){
-            alert("No Change");
+    };
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send();
+}
+function getPlazaTodosTwo() {
+    
+    var xmlHttp = new XMLHttpRequest();
+    
+    var url = "https://3.basecampapi.com/4191003/projects/recordings.json?type=Todo&bucket=11349404&page=2&access_token=BAhbB0kiAbB7ImNsaWVudF9pZCI6IjBmN2U2NjJlMTZkNmMyMDNmOGYyZWFlOWQ3ZTZjYzU4NTRkNDYyMGQiLCJleHBpcmVzX2F0IjoiMjAxOS0wNC0yNVQyMTozNDo1OVoiLCJ1c2VyX2lkcyI6WzM5MTQ3Mzk5XSwidmVyc2lvbiI6MSwiYXBpX2RlYWRib2x0IjoiZWRlNThiNDk4MDhkNmFlMTQ2YjcyMDkzNjNlNGRjNDkifQY6BkVUSXU6CVRpbWUNNc8dwMiVtIsJOg1uYW5vX251bWkCUQE6DW5hbm9fZGVuaQY6DXN1Ym1pY3JvIgczcDoJem9uZUkiCFVUQwY7AEY=--2397b353eb46b3b3a25f9a8f3fe99ea9fb49534b";
+
+    xmlHttp.onreadystatechange = function () {
+      
+        if (this.status === 200){
+            localStorage.setItem("PlazaTwo", this.response);
+        }
+    };
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send();
+}
+function getPlazaTodosThree() {
+    
+    var xmlHttp = new XMLHttpRequest();
+    
+    var url = "https://3.basecampapi.com/4191003/projects/recordings.json?type=Todo&bucket=11349404&page=3&access_token=BAhbB0kiAbB7ImNsaWVudF9pZCI6IjBmN2U2NjJlMTZkNmMyMDNmOGYyZWFlOWQ3ZTZjYzU4NTRkNDYyMGQiLCJleHBpcmVzX2F0IjoiMjAxOS0wNC0yNVQyMTozNDo1OVoiLCJ1c2VyX2lkcyI6WzM5MTQ3Mzk5XSwidmVyc2lvbiI6MSwiYXBpX2RlYWRib2x0IjoiZWRlNThiNDk4MDhkNmFlMTQ2YjcyMDkzNjNlNGRjNDkifQY6BkVUSXU6CVRpbWUNNc8dwMiVtIsJOg1uYW5vX251bWkCUQE6DW5hbm9fZGVuaQY6DXN1Ym1pY3JvIgczcDoJem9uZUkiCFVUQwY7AEY=--2397b353eb46b3b3a25f9a8f3fe99ea9fb49534b";
+
+    xmlHttp.onreadystatechange = function () {
+      
+        if (this.status === 200){
+            localStorage.setItem("PlazaThree", this.response);
+        }
+    };
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send();
+}
+function getPlazaTodosFour() {
+    
+    var xmlHttp = new XMLHttpRequest();
+    
+    var url = "https://3.basecampapi.com/4191003/projects/recordings.json?type=Todo&bucket=11349404&page=4&access_token=BAhbB0kiAbB7ImNsaWVudF9pZCI6IjBmN2U2NjJlMTZkNmMyMDNmOGYyZWFlOWQ3ZTZjYzU4NTRkNDYyMGQiLCJleHBpcmVzX2F0IjoiMjAxOS0wNC0yNVQyMTozNDo1OVoiLCJ1c2VyX2lkcyI6WzM5MTQ3Mzk5XSwidmVyc2lvbiI6MSwiYXBpX2RlYWRib2x0IjoiZWRlNThiNDk4MDhkNmFlMTQ2YjcyMDkzNjNlNGRjNDkifQY6BkVUSXU6CVRpbWUNNc8dwMiVtIsJOg1uYW5vX251bWkCUQE6DW5hbm9fZGVuaQY6DXN1Ym1pY3JvIgczcDoJem9uZUkiCFVUQwY7AEY=--2397b353eb46b3b3a25f9a8f3fe99ea9fb49534b";
+
+    xmlHttp.onreadystatechange = function () {
+      
+        if (this.status === 200){
+            localStorage.setItem("PlazaFour", this.response);
         }
     };
     
-    // Send XMLHttpRequest
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send();
+}
+function getPlazaTodosFive() {
+    
+    var xmlHttp = new XMLHttpRequest();
+    
+    var url = "https://3.basecampapi.com/4191003/projects/recordings.json?type=Todo&bucket=11349404&page=5&access_token=BAhbB0kiAbB7ImNsaWVudF9pZCI6IjBmN2U2NjJlMTZkNmMyMDNmOGYyZWFlOWQ3ZTZjYzU4NTRkNDYyMGQiLCJleHBpcmVzX2F0IjoiMjAxOS0wNC0yNVQyMTozNDo1OVoiLCJ1c2VyX2lkcyI6WzM5MTQ3Mzk5XSwidmVyc2lvbiI6MSwiYXBpX2RlYWRib2x0IjoiZWRlNThiNDk4MDhkNmFlMTQ2YjcyMDkzNjNlNGRjNDkifQY6BkVUSXU6CVRpbWUNNc8dwMiVtIsJOg1uYW5vX251bWkCUQE6DW5hbm9fZGVuaQY6DXN1Ym1pY3JvIgczcDoJem9uZUkiCFVUQwY7AEY=--2397b353eb46b3b3a25f9a8f3fe99ea9fb49534b";
+
+    xmlHttp.onreadystatechange = function () {
+      
+        if (this.status === 200){
+            localStorage.setItem("PlazaFive", this.response);
+        }
+    };
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send();
+}
+function getPlazaTodosSix() {
+    
+    var xmlHttp = new XMLHttpRequest();
+    
+    var url = "https://3.basecampapi.com/4191003/projects/recordings.json?type=Todo&bucket=11349404&page=6&access_token=BAhbB0kiAbB7ImNsaWVudF9pZCI6IjBmN2U2NjJlMTZkNmMyMDNmOGYyZWFlOWQ3ZTZjYzU4NTRkNDYyMGQiLCJleHBpcmVzX2F0IjoiMjAxOS0wNC0yNVQyMTozNDo1OVoiLCJ1c2VyX2lkcyI6WzM5MTQ3Mzk5XSwidmVyc2lvbiI6MSwiYXBpX2RlYWRib2x0IjoiZWRlNThiNDk4MDhkNmFlMTQ2YjcyMDkzNjNlNGRjNDkifQY6BkVUSXU6CVRpbWUNNc8dwMiVtIsJOg1uYW5vX251bWkCUQE6DW5hbm9fZGVuaQY6DXN1Ym1pY3JvIgczcDoJem9uZUkiCFVUQwY7AEY=--2397b353eb46b3b3a25f9a8f3fe99ea9fb49534b";
+
+    xmlHttp.onreadystatechange = function () {
+      
+        if (this.status === 200){
+            localStorage.setItem("PlazaSix", this.response);
+        }
+    };
+    
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send();
+}
+function getPlazaTodosSeven() {
+    
+    var xmlHttp = new XMLHttpRequest();
+    
+    var url = "https://3.basecampapi.com/4191003/projects/recordings.json?type=Todo&bucket=11349404&page=7&access_token=BAhbB0kiAbB7ImNsaWVudF9pZCI6IjBmN2U2NjJlMTZkNmMyMDNmOGYyZWFlOWQ3ZTZjYzU4NTRkNDYyMGQiLCJleHBpcmVzX2F0IjoiMjAxOS0wNC0yNVQyMTozNDo1OVoiLCJ1c2VyX2lkcyI6WzM5MTQ3Mzk5XSwidmVyc2lvbiI6MSwiYXBpX2RlYWRib2x0IjoiZWRlNThiNDk4MDhkNmFlMTQ2YjcyMDkzNjNlNGRjNDkifQY6BkVUSXU6CVRpbWUNNc8dwMiVtIsJOg1uYW5vX251bWkCUQE6DW5hbm9fZGVuaQY6DXN1Ym1pY3JvIgczcDoJem9uZUkiCFVUQwY7AEY=--2397b353eb46b3b3a25f9a8f3fe99ea9fb49534b";
+
+    xmlHttp.onreadystatechange = function () {
+      
+        if (this.status === 200){
+            localStorage.setItem("PlazaSeven", this.response);
+        }
+    };
+    
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send();
+}
+function getPlazaTodosEight() {
+    
+    var xmlHttp = new XMLHttpRequest();
+    
+    var url = "https://3.basecampapi.com/4191003/projects/recordings.json?type=Todo&bucket=11349404&page=8&access_token=BAhbB0kiAbB7ImNsaWVudF9pZCI6IjBmN2U2NjJlMTZkNmMyMDNmOGYyZWFlOWQ3ZTZjYzU4NTRkNDYyMGQiLCJleHBpcmVzX2F0IjoiMjAxOS0wNC0yNVQyMTozNDo1OVoiLCJ1c2VyX2lkcyI6WzM5MTQ3Mzk5XSwidmVyc2lvbiI6MSwiYXBpX2RlYWRib2x0IjoiZWRlNThiNDk4MDhkNmFlMTQ2YjcyMDkzNjNlNGRjNDkifQY6BkVUSXU6CVRpbWUNNc8dwMiVtIsJOg1uYW5vX251bWkCUQE6DW5hbm9fZGVuaQY6DXN1Ym1pY3JvIgczcDoJem9uZUkiCFVUQwY7AEY=--2397b353eb46b3b3a25f9a8f3fe99ea9fb49534b";
+
+    xmlHttp.onreadystatechange = function () {
+      
+        if (this.status === 200){
+            localStorage.setItem("PlazaEight", this.response);
+        }
+    };
+    
     xmlHttp.open("GET", url, true);
     xmlHttp.send();
 }
 
-function sendPlazaRequest() {
+function getBigbyTodos() {
     
-    document.getElementById("display").innerHTML = "";
-    // Create an XMLHttpRequest Object
     var xmlHttp = new XMLHttpRequest();
-    var url = "https://3.basecampapi.com/4191003/buckets/11349404/todosets/1662772918/todolists.json?access_token=BAhbB0kiAbB7ImNsaWVudF9pZCI6IjBmN2U2NjJlMTZkNmMyMDNmOGYyZWFlOWQ3ZTZjYzU4NTRkNDYyMGQiLCJleHBpcmVzX2F0IjoiMjAxOS0wNC0wOVQyMDoyMTowM1oiLCJ1c2VyX2lkcyI6WzM5MTQ3Mzk5XSwidmVyc2lvbiI6MSwiYXBpX2RlYWRib2x0IjoiZWRlNThiNDk4MDhkNmFlMTQ2YjcyMDkzNjNlNGRjNDkifQY6BkVUSXU6CVRpbWUNNM0dwM3NO1QJOg1uYW5vX251bWkClAM6DW5hbm9fZGVuaQY6DXN1Ym1pY3JvIgeRYDoJem9uZUkiCFVUQwY7AEY=--e81cb35aefe218064d4ee49efd76d861a9dfa547";
+    var url = "https://3.basecampapi.com/4191003/projects/recordings.json?type=Todo&bucket=11348667&access_token=BAhbB0kiAbB7ImNsaWVudF9pZCI6IjBmN2U2NjJlMTZkNmMyMDNmOGYyZWFlOWQ3ZTZjYzU4NTRkNDYyMGQiLCJleHBpcmVzX2F0IjoiMjAxOS0wNC0yNVQyMTozNDo1OVoiLCJ1c2VyX2lkcyI6WzM5MTQ3Mzk5XSwidmVyc2lvbiI6MSwiYXBpX2RlYWRib2x0IjoiZWRlNThiNDk4MDhkNmFlMTQ2YjcyMDkzNjNlNGRjNDkifQY6BkVUSXU6CVRpbWUNNc8dwMiVtIsJOg1uYW5vX251bWkCUQE6DW5hbm9fZGVuaQY6DXN1Ym1pY3JvIgczcDoJem9uZUkiCFVUQwY7AEY=--2397b353eb46b3b3a25f9a8f3fe99ea9fb49534b";
 
-    // Parse JSON response
     xmlHttp.onreadystatechange = function () {
+      
         if (this.status === 200){
             var data = JSON.parse(this.response);
-            var nextPage = xmlHttp.getResponseHeader("link");
-            console.log(nextPage);
-            console.log(data);
             for (var i = 0; i < data.length; i++){
-                document.getElementById("display").innerHTML += data[i].title + "<br><br>";
-            }
-        }
-        else if(this.status === 304){
-            alert("No Change");
+                console.log("Item: " + data[i].title);
+                console.log("Unit: " + data[i].parent.title);
+                console.log("Completed?: " + data[i].completed); 
+                console.log("////////////");
+            } 
         }
     };
     
-    // Send XMLHttpRequest
     xmlHttp.open("GET", url, true);
     xmlHttp.send();
-    sendPlazaRequestTwo();
 }
 
-function sendPlazaRequestTwo() {
-    // Create an XMLHttpRequest Object
+function getWestgateTodos() {
+    
     var xmlHttp = new XMLHttpRequest();
-    var url = "https://3.basecampapi.com/4191003/buckets/11349404/todosets/1662772918/todolists.json?page=2&access_token=BAhbB0kiAbB7ImNsaWVudF9pZCI6IjBmN2U2NjJlMTZkNmMyMDNmOGYyZWFlOWQ3ZTZjYzU4NTRkNDYyMGQiLCJleHBpcmVzX2F0IjoiMjAxOS0wNC0wOVQyMDoyMTowM1oiLCJ1c2VyX2lkcyI6WzM5MTQ3Mzk5XSwidmVyc2lvbiI6MSwiYXBpX2RlYWRib2x0IjoiZWRlNThiNDk4MDhkNmFlMTQ2YjcyMDkzNjNlNGRjNDkifQY6BkVUSXU6CVRpbWUNNM0dwM3NO1QJOg1uYW5vX251bWkClAM6DW5hbm9fZGVuaQY6DXN1Ym1pY3JvIgeRYDoJem9uZUkiCFVUQwY7AEY=--e81cb35aefe218064d4ee49efd76d861a9dfa547";
+    var url = "https://3.basecampapi.com/4191003/projects/recordings.json?type=Todo&bucket=11348677&access_token=BAhbB0kiAbB7ImNsaWVudF9pZCI6IjBmN2U2NjJlMTZkNmMyMDNmOGYyZWFlOWQ3ZTZjYzU4NTRkNDYyMGQiLCJleHBpcmVzX2F0IjoiMjAxOS0wNC0yNVQyMTozNDo1OVoiLCJ1c2VyX2lkcyI6WzM5MTQ3Mzk5XSwidmVyc2lvbiI6MSwiYXBpX2RlYWRib2x0IjoiZWRlNThiNDk4MDhkNmFlMTQ2YjcyMDkzNjNlNGRjNDkifQY6BkVUSXU6CVRpbWUNNc8dwMiVtIsJOg1uYW5vX251bWkCUQE6DW5hbm9fZGVuaQY6DXN1Ym1pY3JvIgczcDoJem9uZUkiCFVUQwY7AEY=--2397b353eb46b3b3a25f9a8f3fe99ea9fb49534b";
 
-    // Parse JSON response
     xmlHttp.onreadystatechange = function () {
+      
         if (this.status === 200){
             var data = JSON.parse(this.response);
-            var nextPage = xmlHttp.getResponseHeader("link");
-            console.log(nextPage);
-            console.log(data);
             for (var i = 0; i < data.length; i++){
-                document.getElementById("display").innerHTML += data[i].title + "<br><br>";
-            }
-        }
-        else if(this.status === 304){
-            alert("No Change");
+                console.log("Item: " + data[i].title);
+                console.log("Unit: " + data[i].parent.title);
+                console.log("Completed?: " + data[i].completed); 
+                console.log("////////////");
+            } 
         }
     };
     
-    // Send XMLHttpRequest
     xmlHttp.open("GET", url, true);
     xmlHttp.send();
-    sendPlazaRequestThree();
 }
 
-function sendPlazaRequestThree() {
-    // Create an XMLHttpRequest Object
+function getWalnutTodos() {
+    
     var xmlHttp = new XMLHttpRequest();
-    var url = "https://3.basecampapi.com/4191003/buckets/11349404/todosets/1662772918/todolists.json?page=3&access_token=BAhbB0kiAbB7ImNsaWVudF9pZCI6IjBmN2U2NjJlMTZkNmMyMDNmOGYyZWFlOWQ3ZTZjYzU4NTRkNDYyMGQiLCJleHBpcmVzX2F0IjoiMjAxOS0wNC0wOVQyMDoyMTowM1oiLCJ1c2VyX2lkcyI6WzM5MTQ3Mzk5XSwidmVyc2lvbiI6MSwiYXBpX2RlYWRib2x0IjoiZWRlNThiNDk4MDhkNmFlMTQ2YjcyMDkzNjNlNGRjNDkifQY6BkVUSXU6CVRpbWUNNM0dwM3NO1QJOg1uYW5vX251bWkClAM6DW5hbm9fZGVuaQY6DXN1Ym1pY3JvIgeRYDoJem9uZUkiCFVUQwY7AEY=--e81cb35aefe218064d4ee49efd76d861a9dfa547";
+    var url = "https://3.basecampapi.com/4191003/projects/recordings.json?type=Todo&bucket=11348681&access_token=BAhbB0kiAbB7ImNsaWVudF9pZCI6IjBmN2U2NjJlMTZkNmMyMDNmOGYyZWFlOWQ3ZTZjYzU4NTRkNDYyMGQiLCJleHBpcmVzX2F0IjoiMjAxOS0wNC0yNVQyMTozNDo1OVoiLCJ1c2VyX2lkcyI6WzM5MTQ3Mzk5XSwidmVyc2lvbiI6MSwiYXBpX2RlYWRib2x0IjoiZWRlNThiNDk4MDhkNmFlMTQ2YjcyMDkzNjNlNGRjNDkifQY6BkVUSXU6CVRpbWUNNc8dwMiVtIsJOg1uYW5vX251bWkCUQE6DW5hbm9fZGVuaQY6DXN1Ym1pY3JvIgczcDoJem9uZUkiCFVUQwY7AEY=--2397b353eb46b3b3a25f9a8f3fe99ea9fb49534b";
 
-    // Parse JSON response
     xmlHttp.onreadystatechange = function () {
+      
         if (this.status === 200){
             var data = JSON.parse(this.response);
-            var nextPage = xmlHttp.getResponseHeader("link");
-            console.log(nextPage);
-            console.log(data);
             for (var i = 0; i < data.length; i++){
-                document.getElementById("display").innerHTML += data[i].title + "<br><br>";
-            }
-        }
-        else if(this.status === 304){
-            alert("No Change");
-        }
-    };
-    
-    // Send XMLHttpRequest
-    xmlHttp.open("GET", url, true);
-    xmlHttp.send();
-    sendPlazaRequestFour();
-}
-
-function sendPlazaRequestFour() {
-    // Create an XMLHttpRequest Object
-    var xmlHttp = new XMLHttpRequest();
-    var url = "https://3.basecampapi.com/4191003/buckets/11349404/todosets/1662772918/todolists.json?page=4&access_token=BAhbB0kiAbB7ImNsaWVudF9pZCI6IjBmN2U2NjJlMTZkNmMyMDNmOGYyZWFlOWQ3ZTZjYzU4NTRkNDYyMGQiLCJleHBpcmVzX2F0IjoiMjAxOS0wNC0wOVQyMDoyMTowM1oiLCJ1c2VyX2lkcyI6WzM5MTQ3Mzk5XSwidmVyc2lvbiI6MSwiYXBpX2RlYWRib2x0IjoiZWRlNThiNDk4MDhkNmFlMTQ2YjcyMDkzNjNlNGRjNDkifQY6BkVUSXU6CVRpbWUNNM0dwM3NO1QJOg1uYW5vX251bWkClAM6DW5hbm9fZGVuaQY6DXN1Ym1pY3JvIgeRYDoJem9uZUkiCFVUQwY7AEY=--e81cb35aefe218064d4ee49efd76d861a9dfa547";
-
-    // Parse JSON response
-    xmlHttp.onreadystatechange = function () {
-        if (this.status === 200){
-            var data = JSON.parse(this.response);
-            var nextPage = xmlHttp.getResponseHeader("link");
-            console.log(nextPage);
-            console.log(data);
-            for (var i = 0; i < data.length; i++){
-                document.getElementById("display").innerHTML += data[i].title + "<br><br>";
-            }
-        }
-        else if(this.status === 304){
-            alert("No Change");
+                console.log("Item: " + data[i].title);
+                console.log("Unit: " + data[i].parent.title);
+                console.log("Completed?: " + data[i].completed); 
+                console.log("////////////");
+            } 
         }
     };
     
-    // Send XMLHttpRequest
     xmlHttp.open("GET", url, true);
     xmlHttp.send();
-}
-
-function plazaItemRequest(listId) {
-    // Create an XMLHttpRequest Object
-    var xmlHttp = new XMLHttpRequest();
-    var url = "https://3.basecampapi.com/4191003/buckets/11349404/todolists/" + listId + "/todos.json?access_token=BAhbB0kiAbB7ImNsaWVudF9pZCI6IjBmN2U2NjJlMTZkNmMyMDNmOGYyZWFlOWQ3ZTZjYzU4NTRkNDYyMGQiLCJleHBpcmVzX2F0IjoiMjAxOS0wNC0wOVQyMDoyMTowM1oiLCJ1c2VyX2lkcyI6WzM5MTQ3Mzk5XSwidmVyc2lvbiI6MSwiYXBpX2RlYWRib2x0IjoiZWRlNThiNDk4MDhkNmFlMTQ2YjcyMDkzNjNlNGRjNDkifQY6BkVUSXU6CVRpbWUNNM0dwM3NO1QJOg1uYW5vX251bWkClAM6DW5hbm9fZGVuaQY6DXN1Ym1pY3JvIgeRYDoJem9uZUkiCFVUQwY7AEY=--e81cb35aefe218064d4ee49efd76d861a9dfa547";
-
-    // Parse JSON response
-    xmlHttp.onreadystatechange = function () {
-        if (this.status === 200){
-            var data = JSON.parse(this.response);
-            console.log(data);
-            plazaCallback(data);
-        }
-    };
-    
-    // Send XMLHttpRequest
-    xmlHttp.open("GET", url, true);
-    xmlHttp.send();
-    
-    return true;
-}
-
-function plazaCallback(data){
-    
-    for (var i = 0; i < data.length; i++){
-        document.getElementById("display").innerHTML += data[i].title + "<br>"; 
-    }
-}
-
-function display(data) {
-    var i = 0;
-    document.getElementById("display").innerHTML = "";
-    for (i; i < data.length; i++){
-        document.getElementById("display").innerHTML += data[i].title + "<br><br>";
-    }
 }
---
+++ report.js
@@ -0,0 +1,24 @@
+function generateReport() {
+    var data = [];
+    for (var i = 0; i < localStorage.length; i++){
+        data[i] = JSON.parse(localStorage.getItem(localStorage.key(i)));
+    }
+
+    var count = 0;
+
+
+    for (let text of data){
+        for (var j = 0; j < text.length; j++){
+            var table = document.getElementById("punch");
+            var row = table.insertRow(j+1);
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
+            var cell3 = row.insertCell(2);
+
+            cell1.innerHTML = text[j].parent.title;
+            cell2.innerHTML = text[j].title;
+            cell3.innerHTML = text[j].completed;
+        }
+    }
+    console.log(count);
+}
